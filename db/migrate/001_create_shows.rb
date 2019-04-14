e79c08ef263277020b2cd923f2d31c71ee76a7d3
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-14 23:48:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,10 @@
 class CreateShowes < ActiveRecord::Migration[5.2]
   def change
     create_table :showes do |t|
-      t.string   :title
-      t.integer  :release_date
-      t.string   :director
-      t.string   :lead
-      t.boolean  :in_theaters
+      t.string   :name
+      t.string  :network
+      t.string  :day
+      t.integer :rating
   end
   end
 end
